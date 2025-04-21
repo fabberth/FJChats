72c434e369f7f2372a23790109af07ab8ffc3f54
fabberth <fabberthjuniortorres@hotmail.com>
2025-04-21 17:33:08 -0500

    upload the project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+
+    { path: '**', component: HomeComponent },
+    // otras rutas si querés
+];
